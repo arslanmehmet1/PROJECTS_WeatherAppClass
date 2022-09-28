53f88cb53a481c5228e60bb05360f7c479999b44
Mehmet Arslan <marslan.developer@gmail.com>
2022-09-28 22:06:47 +0300

    second lesson finished

--- weather.js
+++ weather.js
@@ -16,7 +16,55 @@ const getWeatherDataFromApi = async () => {
   const units = "metric";
   const lang = "tr";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${tokenKey}&units=${units}&lang=${lang}`;
+  try {
+    const response = await fetch(url).then((res) => res.json());
+    console.log(response);
+    const { main, sys, weather, name } = response;
 
-  const response = await fetch(url).then((res) => res.json());
-  console.log(response);
+    const iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+    const iconUrlAWS = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
+
+    const cityNameSpans = document.querySelectorAll(".city span");
+    const cityNameSpansArray = Array.from(cityNameSpans);
+    if (cityNameSpansArray.length > 0) {
+      const filteredArray = cityNameSpansArray.filter(
+        (span) => span.innerText == name
+      );
+      if (filteredArray.length > 0) {
+        msg.innerText = `You already know `;
+        setTimeout(() => {
+          msg.innerText = "";
+        }, 5000);
+        form.reset();
+        return;
+      }
+    }
+
+    const createdLi = document.createElement("li");
+    createdLi.classList.add("city");
+    createdLi.innerHTML = `<h2 class="city-name" data-name="${name}, ${
+      sys.country
+    }">
+                              <span>${name}</span>
+                              <sup>${sys.country}</sup>
+                        </h2>
+                        <div class="city-temp">${Math.round(
+                          main.temp
+                        )}<sup>°C</sup></div>
+                              <figure>
+                                  <img class="city-icon" src="${iconUrl}">
+                                  <figcaption>${
+                                    weather[0].description
+                                  }</figcaption>
+                              </figure>`;
+    list.prepend(createdLi);
+    form.reset();
+  } catch (error) {
+    console.log(error);
+    msg.innerText = `404 (City Not Found)`;
+    setTimeout(() => {
+      msg.innerText = "";
+    }, 5000);
+    form.reset();
+  }
 };
